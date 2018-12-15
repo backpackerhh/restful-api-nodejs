36d98ed8ef1804da161f6e19f0bb0873a694c222
David Montesdeoca <backpackerhh@gmail.com>
2018-12-15 21:02:06 +0100

    Add basic HTTP server

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World!')
+/*
+ * Primary file for the API
+ *
+ */
+
+// Dependencies
+const http = require('http');
+
+// Server
+const server = http.createServer((request, response) => {
+  response.end('Hello World!\n');
+});
+
+server.listen(3000, () => {
+  console.log('The server is listening on port 3000 now');
+});
