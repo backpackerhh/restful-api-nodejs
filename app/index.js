3b2f30633bde7d1ca07275464d103fff1f6a1d57
David Montesdeoca <backpackerhh@gmail.com>
2018-12-15 22:28:02 +0100

    Allow server to parse request paths

--- app/index.js
+++ app/index.js
@@ -5,10 +5,17 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // Server
 const server = http.createServer((request, response) => {
+  const parsedURL = url.parse(request.url, true);
+  const trimmedPath = parsedURL.pathname.replace(/^\/+|\/+$/g, '');
+
   response.end('Hello World!\n');
+
+  console.log('Request received!');
+  console.log(`Path: ${trimmedPath}`);
 });
 
 server.listen(3000, () => {
