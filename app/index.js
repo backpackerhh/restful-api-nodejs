00bcb012877a8117816474328360f2e347a65e6b
David Montesdeoca <backpackerhh@gmail.com>
2018-12-16 23:28:38 +0100

    Allow server to parse payload

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // Server
 const server = http.createServer((request, response) => {
@@ -14,14 +15,25 @@ const server = http.createServer((request, response) => {
   const httpMethod = request.method;
   const queryStringObject = parsedURL.query;
   const headers = request.headers;
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
 
-  response.end('Hello World!\n');
+  request.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
 
-  console.log('Request received!');
-  console.log(`Path: ${trimmedPath}`);
-  console.log(`HTTP method: ${httpMethod}`);
-  console.log('Query string parameters:', queryStringObject);
-  console.log('Headers:', headers);
+  request.on('end', () => {
+    buffer += decoder.end();
+
+    response.end('Hello World!\n');
+
+    console.log('Request received!');
+    console.log(`Path: ${trimmedPath}`);
+    console.log(`HTTP method: ${httpMethod}`);
+    console.log('Query string parameters:', queryStringObject);
+    console.log('Headers:', headers);
+    console.log(`Payload: ${buffer}`);
+  });
 });
 
 server.listen(3000, () => {
